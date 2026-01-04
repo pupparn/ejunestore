45c9be7d6549304e6ef4535b6f0970dedeaadb11
pupparn <natthawat.wk@gmail.com>
2026-01-04 21:03:28 +0700

    fix config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "/ejunestore/",
+  base: process.env.VITE_BASE_URL || "/ejunestore/",
 });
